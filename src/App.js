2b841525dcee7f181397649baf3b1120dbbec29e
Anouk Montreuil <anoukmontreuil@gmail.com>
2018-02-06 17:04:42 -0500

    Completing Question 4...

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">...Meh...</h1>
         </header>
         <p className="App-intro">
-          Question 3: Solved!
+          Question 4: Solved!
         </p>
       </div>
     );
